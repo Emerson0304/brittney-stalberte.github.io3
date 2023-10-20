4a2a33a0cf2dbf843e196823c986ec17a89d526f
Brittney Stalberte <emersonb3445@gmail.com>
2023-10-20 18:00:10 +0000

    did a little work in the studies project on loops

---
+++ Studies/loops.js
@@ -0,0 +1,33 @@
+/**
+ * LOOPS:  Loops are used to perform repeated tasks based on a conditions typically
+ * return true or false. A loop will continue running until the defined condition returns false.
+ * 
+ * 0: While loop: The while loop starts by evaluating condition. If condition evaluates to true,
+ * the code in the code block gets executed. If condition is false, 
+ * the code in the code block is not executed and the loop ends.  
+ *
+ */
+
+    // 1. While loop
+/*
+0: While loop: The while loop starts by evaluating condition. If condition evaluates to true,
+the code in the code block gets executed. If condition is false, 
+the code in the code block is not executed and the loop ends.
+*/  
+    let count = 1; 
+    while(count < 5){
+    console.log(count);
+        i++; // <== IMPORTANT 
+    } // => 1 2 3 4 
+
+
+
+/*
+1: 
+
+
+*/
+
+
+  
+ 
